76e5042003843a986276bc6d742290261fdd1ca2
Evan Sizemore <evan@wool.homes>
2024-08-20 18:07:11 -0400

    added chown to script

--- script/install_assets.sh
+++ script/install_assets.sh
@@ -9,6 +9,8 @@ function bundle_config_and_install() {
   bundle config --global build.nokogiri --use-system-libraries &&
   bundle config --global build.ffi --enable-system-libffi &&
   mkdir -p /home/docker/.bundle &&
+  sudo chown -R docker:docker /usr/src/app
+  sudo chmod -R o+rwx .
   bundle install --jobs $(nproc)
 }
 
